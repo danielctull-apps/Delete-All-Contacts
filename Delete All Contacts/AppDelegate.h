e2403bb00e95fba74c5479456e2f6401ef888c5b
Daniel Tull <dt@danieltull.co.uk>
2011-10-13 23:45:34 +0100

    Initial Commit

---
+++ Delete All Contacts/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Delete All Contacts
+//
+//  Created by Daniel Tull on 13.10.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Delete All Contacts/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delete All Contacts
+//
+//  Created by Daniel Tull on 13.10.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
